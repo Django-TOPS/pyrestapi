d4465f2e2b21a89777ec7a64093a168de66a4c2b
Django-TOPS <djangotestmail2021@gmail.com>
2022-07-15 14:27:45 +0530

    Add Req. File

--- CRUDAPI/settings.py
+++ CRUDAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0!st7r8$^cvyion_z9-a&qk$!2afv7ko7rf(ek)hm)b(a$xkbr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
